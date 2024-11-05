0f1cff0abb1944b6bf738956ff716744ca5c9116
hamedelahish <134408686+hamedelahish@users.noreply.github.com>
2024-11-05 04:05:04 +0330

    add search component to header

--- src/app/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard-layout/dashboard-layout.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from "../shared/services/auth/auth.service";
 import {Observable, shareReplay} from "rxjs";
 import {selectCartCount} from "../store/cart/cart.selectors";
 import {Store} from "@ngrx/store";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -10,15 +11,34 @@ import {Store} from "@ngrx/store";
   styleUrls: ['./dashboard-layout.component.scss']
 })
 export class DashboardLayoutComponent implements OnInit {
-  isLoggedIn$=this.authService.isLoggedIn$.pipe(shareReplay(1))
+  isLoggedIn$ = this.authService.isLoggedIn$.pipe(shareReplay(1))
   cartCount$: Observable<number> | undefined;
+  searchText: string = '';
 
-  constructor(private authService:AuthService,private store:Store ) { }
+  constructor(private authService: AuthService, private store: Store, private router: Router) {
+  }
 
   ngOnInit(): void {
-    this.cartCount$=this.store.select(selectCartCount)
+    this.cartCount$ = this.store.select(selectCartCount)
+
   }
-  logout(){
+
+  logout() {
     this.authService.logout()
+  }
+
+  search(event: any): void {
+    if (event.key === 'Enter' || event.type === 'click') {
+      this.searchText = event.target.value;
+      if (this.searchText.length > 2) {
+        this.router.navigate(['/dashboard'], {queryParams: {search: this.searchText}});
+      }
+    }
+  }
+
+  clearSearch(): void {
+    this.searchText = '';
+    this.router.navigate(['/dashboard']);
+
   }
 }
